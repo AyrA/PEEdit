0feb1a5a7002813e78e5857cf1efbd79492ce550
Kevin Gut <kevin.gut@bitm.sg>
2018-08-16 00:53:09 +0200

    Assembly Description

--- BinVer/Properties/AssemblyInfo.cs
+++ BinVer/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BinVer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("PE Header reader utility")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("AyrA.ch")]
 [assembly: AssemblyProduct("BinVer")]
